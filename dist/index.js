dec922c2d689636215c8dd43eba4a90dc5062610
Bryan Martinez <a023qwse@gmail.com>
2022-04-13 11:47:33 -0400

    checkpoint

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+// BASIC TYPES
+let id = 5;
+let company = 'Traversy Media';
+let isPublished = true;
+let x = 'Hello';
+x = true;
+let age;
+let ids = [12, 2, 3, 4, 44];
+// Union
+let pid;
